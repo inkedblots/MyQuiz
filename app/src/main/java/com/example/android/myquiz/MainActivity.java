84ee3c4ab2a1036900cba5b1977a57530b91b544
inkedblots <inkedblots@hotmail,com>
2018-04-30 17:45:56 -0700

    changes: formatting text size added image header added horizontal line between questions centered submit/reset buttons refactored code

--- app/src/main/java/com/example/android/myquiz/MainActivity.java
+++ app/src/main/java/com/example/android/myquiz/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.android.myquiz;
 
-
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -11,7 +10,6 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         // Hide the keyboard
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
@@ -50,9 +47,7 @@ public class MainActivity extends AppCompatActivity {
     //Calculate the total score
     private int getTotalScore() {
 
-
         // Question 1 EditText - Correct Answer is (Second Degree)
-
         EditText questionOneAnswer = findViewById(R.id.questionOne_answer);
         boolean answer1;
         String q1s = questionOneAnswer.getText().toString();
@@ -61,16 +56,10 @@ public class MainActivity extends AppCompatActivity {
             return -1;
         }
 
-        if (q1s.equalsIgnoreCase("second degree") || q1s.equals("2nd degree")) {
-            answer1 = true;
-        } else {
-            answer1 = false;
-        }
-
+        answer1 = q1s.equalsIgnoreCase("second degree") || q1s.equals("2nd degree");
 
         // Question 2 Radio - Correct Answer is 2 (Ligaments)
         Boolean answer2 = qTwoRbTwo.isChecked();
-
 
         // Question 3  Checkbox - Correct Answers are 1 (Fingerprints) and 3 (Toeprints)
         Boolean isAnswer3_choice1 = qThreeCbOne.isChecked();
@@ -79,10 +68,8 @@ public class MainActivity extends AppCompatActivity {
         Boolean isAnswer3_choice4 = qThreeCbFour.isChecked();
         Boolean answer3 = isAnswer3_choice1 && isAnswer3_choice3 && !isAnswer3_choice2 && !isAnswer3_choice4;
 
-
         // Question 4 RadioButton - Correct Answer is 4 (Sensory Reception)
         Boolean answer4 = qFourRbThree.isChecked();
-
 
         // Question 5  Checkbox- Correct Answer is 4 (Hemopoiesis)
         Boolean isAnswer5_choice1 = qFiveCbOne.isChecked();
@@ -91,23 +78,33 @@ public class MainActivity extends AppCompatActivity {
         Boolean isAnswer5_choice4 = qFiveCbFour.isChecked();
         Boolean answer5 = !isAnswer5_choice1 && !isAnswer5_choice2 && !isAnswer5_choice3 && isAnswer5_choice4;
 
-
         // Question 6:  RadioButton, Correct Answer 2 (Long Bone)
         Boolean answer6 = qSixRbTwo.isChecked();
 
-
         //  Scoring
-        if (answer1) {score++;}
+        if (answer1) {
+            score++;
+        }
 
-        if (answer2) {score++;}
+        if (answer2) {
+            score++;
+        }
 
-        if (answer3) {score++;}
+        if (answer3) {
+            score++;
+        }
 
-        if (answer4) {score++;}
+        if (answer4) {
+            score++;
+        }
 
-        if (answer5) {score++;}
+        if (answer5) {
+            score++;
+        }
 
-        if (answer6) {score++;}
+        if (answer6) {
+            score++;
+        }
 
         return score;
     }
@@ -124,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button submitButton = findViewById(R.id.submitBtn);
         submitButton.setEnabled(false);
-        Toast.makeText(this, String.format(getString(R.string.scoreMessage), finalScore), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, String.format(getString(R.string.scoreMessage), finalScore), Toast.LENGTH_LONG).show();
     }
 
     /* Resets the score to zero */
@@ -133,5 +130,4 @@ public class MainActivity extends AppCompatActivity {
         finish();
         startActivity(intent);
     }
-
 }
